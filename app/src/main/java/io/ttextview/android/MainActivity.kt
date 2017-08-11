d5ef9f3221abd646397925fcfa9a3c3ec66eaafb
Tuon Bondol <bondol@2ntkh.com>
2017-08-11 11:17:18 +0700

    First Commit with TTextView

---
+++ app/src/main/java/io/ttextview/android/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.ttextview.android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/***
+ *
+ * @author TUON BONDOL on 8/11/17.
+ *
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
